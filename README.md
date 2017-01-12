d2ed5298bcc0cd196eac631e10ce25b53652262f
Paulo Pires <taralloqueiroiz@gmail.com>
2017-01-12 09:44:33 +0100

    Initial Structure and basic functions

---
+++ README.md
@@ -0,0 +1,3 @@
+# stripe-coupon
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/CouponController.js
@@ -0,0 +1,50 @@
+var stripe = require('stripe')('sk_test_gvvoWW1EXO2aQiFKnEwIoy3U');
+/**
+ * CouponController
+ *
+ * @description :: Server-side logic for managing coupons
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `CouponController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `CouponController.delete()`
+   */
+  delete: async function (req, res) {
+    stripe.coupons.del(req.params.id)
+      .then(() => {
+        return res.json({
+          todo: 'delete() is not implemented yet!'
+        });
+    })
+  },
+
+
+  /**
+   * `CouponController.list()`
+   */
+  list: function (req, res) {
+    stripe.coupons.list(
+      {},
+      function(err, coupons) {
+        return res.json({
+          coupons: coupons.data
+        });
+      }
+    );
+  }
+};
+
