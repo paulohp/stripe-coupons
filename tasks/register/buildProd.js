4769f3247d3ea4ee797d606dc1ec39fe827b6727
Paulo Pires <taralloqueiroiz@gmail.com>
2017-01-15 13:40:01 +0100

    Fixing prod build

--- tasks/register/buildProd.js
+++ tasks/register/buildProd.js
@@ -19,6 +19,7 @@ module.exports = function(grunt) {
   grunt.registerTask('buildProd', [
     'compileAssets',
     'concat',
+    'uglify',
     'cssmin',
     'linkAssetsBuildProd',
     'clean:build',
